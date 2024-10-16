ee97ee74817cc8395529355cffd73d84a48baf8a
Shiori030 <nnn666nnn4125@gmail.com>
2024-10-16 15:21:27 +0800

    refactor: 刪除無用檔案

--- src/app/homework01/page.js
+++ src/app/homework01/page.js
@@ -2,7 +2,8 @@
 import Dialog from '@/components/dialog'
 import DialogTest from './dialogTest'
 import { useState } from 'react'
-import DialogHeaderTest from './dialogHeaderTest'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export default function Homework1 () {
   const [isOpen, setIsOpen] = useState(false)
@@ -19,7 +20,17 @@ export default function Homework1 () {
       isOpen={isOpen}
       onClose={() => setIsOpen(!isOpen)}
       title="每日論語"
-      customHeader={<DialogHeaderTest>測試用的Header</DialogHeaderTest>}
+      // customHeader={<DialogHeaderTest>測試用的Header</DialogHeaderTest>}
+      customHeader={(onClose) => {
+        return (
+          <div className={'m-5 flex flex-nowrap justify-between border-b-2 border-[#b098986b] pb-1 text-2xl'}>
+            測試用的Header
+            <button className={'text-[#BEBEBE]'} type="button" onClick={onClose}>
+              <FontAwesomeIcon icon={faXmark} />
+            </button>
+          </div>
+        )
+      }}
       customContent={<DialogTest/>}
       />
     </>
